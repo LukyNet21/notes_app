65f54907d6338033f9856739eb90e5a9650b8bec
luky <admin@lukynet21.com>
2024-08-21 11:49:27 +0200

    rename note functionality

--- web/src/scripts/editor.js
+++ web/src/scripts/editor.js
@@ -13,91 +13,104 @@ import Marker from '@editorjs/marker';
 import Quote from '@editorjs/quote';
 
 const editor = new EditorJS({
-  theme: "dark",
-  tools: {
-    header: Header,
-    image: SimpleImage,
-    embed: Embed,
-    table: Table,
-    underline: Underline,
-    code: CodeTool,
-    quote: Quote,
-    checklist: {
-      class: Checklist,
-      inlineToolbar: true,
+    theme: "dark",
+    tools: {
+        header: Header,
+        image: SimpleImage,
+        embed: Embed,
+        table: Table,
+        underline: Underline,
+        code: CodeTool,
+        quote: Quote,
+        checklist: {
+            class: Checklist,
+            inlineToolbar: true,
+        },
+        list: {
+            class: List,
+            inlineToolbar: true,
+            config: {
+                defaultStyle: 'unordered'
+            }
+        },
+        inlineCode: {
+            class: InlineCode,
+            shortcut: 'CMD+SHIFT+M',
+        },
+        warning: {
+            class: Warning,
+            inlineToolbar: true,
+            shortcut: 'CMD+SHIFT+W',
+            config: {
+                titlePlaceholder: 'Title',
+                messagePlaceholder: 'Message',
+            },
+        },
+        marker: {
+            class: Marker,
+            shortcut: 'CMD+SHIFT+M',
+        }
     },
-    list: {
-      class: List,
-      inlineToolbar: true,
-      config: {
-        defaultStyle: 'unordered'
-      }
-    },
-    inlineCode: {
-      class: InlineCode,
-      shortcut: 'CMD+SHIFT+M',
-    },
-    warning: {
-      class: Warning,
-      inlineToolbar: true,
-      shortcut: 'CMD+SHIFT+W',
-      config: {
-        titlePlaceholder: 'Title',
-        messagePlaceholder: 'Message',
-      },
-    },
-    marker: {
-      class: Marker,
-      shortcut: 'CMD+SHIFT+M',
-    }
-  },
 })
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id')
+const noteNameEditInput = document.getElementById("noteNameEditInput")
+const noteNameField = document.getElementById("projectName")
+
+let noteName
 
 editor.isReady.then(() => {
-  fetch(`http://localhost:8080/getNoteByID/${id}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    credentials: "include",
-  })
-    .then((response) => {
-      if (!response.ok) {
-        window.location.replace("/dashboard");
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
+    fetch(`http://localhost:8080/getNoteByID/${id}`, {
+        method: "GET",
+        credentials: "include",
     })
-    .then((data) => {
-      console.log("Success:", data);
-      document.getElementById("projectName").innerText = data.note_name
-      editor.blocks.render(JSON.parse(data.note_content))
-    })
-    .catch((error) => {
-      console.error("Error:", error);
+        .then((response) => {
+            if (!response.ok) {
+                window.location.replace("/dashboard");
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log("Success:", data);
+            noteName = data.note_name
+            noteNameField.innerText = noteName
+            noteNameEditInput.value = noteName
+            editor.blocks.render(JSON.parse(data.note_content))
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+})
+    .catch((reason) => {
+        console.log(`Editor.js initialization failed because of ${reason}`)
     });
-})
-  .catch((reason) => {
-    console.log(`Editor.js initialization failed because of ${reason}`)
-  });
+
 document.getElementById("saveButton").addEventListener("click", () => {
-  editor.save().then((outputData) => {
-    console.log('Article data: ', JSON.stringify(outputData))
-    fetch(`http://localhost:8080/updateNote/${id}`, {
-      method: "PUT",
-      credentials: "include",
-      body: JSON.stringify({
-        "note_name": "Note 1",
-        "note_content": JSON.stringify(outputData)
-      })
+    editor.save().then((outputData) => {
+        console.log('Article data: ', JSON.stringify(outputData))
+        fetch(`http://localhost:8080/updateNote/${id}`, {
+            method: "PUT",
+            credentials: "include",
+            body: JSON.stringify({
+                "note_name": noteName,
+                "note_content": JSON.stringify(outputData)
+            })
+        })
     })
-  }).catch((error) => {
-    console.log('Saving failed: ', error)
-  });
+        .catch((error) => {
+            console.log('Saving failed: ', error)
+        });
 })
 
+document.getElementById("noteNameEditBtn").addEventListener("click", e => {
+    document.getElementById("projectName").classList.toggle("hidden")
+    noteNameEditInput.classList.toggle("hidden")
+})
 
+noteNameEditInput.addEventListener("change", e => {
+    noteName = e.target.value
+    noteNameField.innerText = noteName
+})
